a71f8467ac410cf1e7ace80a3c393af5321dba81
gayathri_kambhampati <gayathri_kambhampati@dell.com>
2020-06-24 13:44:20 +0100

    discovery.1

--- components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
+++ components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
@@ -11,8 +11,9 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestConfig {
-    @LoadBalanced
+
     @Bean
+    @LoadBalanced
     public RestOperations restOperations() {
         return new RestTemplate();
     }
